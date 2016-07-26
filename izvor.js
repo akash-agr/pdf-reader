84442e41579e3983a0b528de90b3029e5f88c497
Damjan <dpavlica@itekako.com>
2016-07-26 20:13:34 +0200

    refaktor

--- izvor.js
+++ izvor.js
@@ -1,63 +1,55 @@
 /*** VARIJABLE ***/
 
+var zoom = 1;
 var id = null;
 var vrsta = null;
 var platno = null;
-var sadrzaj = null;
+var podloga = null;
 var ovajDokument = null;
-var brojStrane = null;
+var brojStrane = 1;
+var fajl_url = 'ctut.pdf';
 
 /*** DOGAĐAJI ***/
 
 window.addEventListener('load', function () {
-  vrsta = 2;
   platno = $('#platno');
-
-  if (vrsta == 2) {
-    platno.width = platno.parentElement.offsetWidth;
-    platno.height = window.innerHeight;
-    sadrzaj = platno.getContext('2d');
-    sadrzaj.font = "bold 16px Arial";
-    sadrzaj.fillText("Dokument se učitava...", platno.width / 2 - 100, 100);
-    brojStrane = 1;
-    ucitajPDF();
-  }
-
+  platno.width = platno.parentElement.offsetWidth;
+  platno.height = window.innerHeight;
+  podloga = platno.getContext('2d');
+  podloga.font = "bold 16px Arial";
+  podloga.fillText("Dokument se učitava...", platno.width / 2 - 100, 100);
+  ucitajPDF(fajl_url);
 }); // on load
 
 document.addEventListener('click', function (e) {
   var element = e.target;
-
   if (element.classList.contains('js-idi-nazad')) idiNazad();
-
   if (element.classList.contains('js-idi-napred')) idiNapred();
-
 }); // on click
 
 /*** FUNKCIJE ***/
 
-function ucitajPDF() {
-  var fajl_url = 'ctut.pdf';
+function ucitajPDF(fajl_url) {
   PDFJS.disableWorker = true; // disable workers to avoid cross-origin issue
   // asinhrono downloaduje PDF kao ArrayBuffer
   PDFJS.getDocument(fajl_url).then(function (_pdfDoc) {
     ovajDokument = _pdfDoc;
     if (brojStrane > ovajDokument.numPages) brojStrane = ovajDokument.numPages;
-    renderujStranu(brojStrane);
+    renderujStranu();
   });
 }
 
-function renderujStranu(broj) {
+function renderujStranu() {
   // koristi promise da fetchuje stranu
-  ovajDokument.getPage(broj).then(function (pdfStrana) {
+  ovajDokument.getPage(brojStrane).then(function (pdfStrana) {
     // prilagodjava se raspoloživoj širini
     var roditeljskaSirina = platno.parentElement.offsetWidth;
-    var viewport = pdfStrana.getViewport(roditeljskaSirina / pdfStrana.getViewport(1.0).width);
+    var viewport = pdfStrana.getViewport(roditeljskaSirina / pdfStrana.getViewport(zoom).width);
     platno.height = viewport.height;
     platno.width = viewport.width;
     // renderuje PDF stranu na platno
     var renderContext = {
-      canvasContext: sadrzaj,
+      canvasContext: podloga,
       viewport: viewport
     };
     pdfStrana.render(renderContext);
--- main.js
+++ main.js
@@ -1,33 +1,35 @@
-var pdfBase64 = 'JVBERi0xLjcKCjEgMCBvYmogICUgZW50cnkgcG9pbnQKPDwKICAvVHlwZSAvQ2F0YWxvZwogIC9QYWdlcyAyIDAgUgo+PgplbmRvYmoKCjIgMCBvYmoKPDwKICAvVHlwZSAvUGFnZXMKICAvTWVkaWFCb3ggWyAwIDAgMjAwIDIwMCBdCiAgL0NvdW50IDEKICAvS2lkcyBbIDMgMCBSIF0KPj4KZW5kb2JqCgozIDAgb2JqCjw8CiAgL1R5cGUgL1BhZ2UKICAvUGFyZW50IDIgMCBSCiAgL1Jlc291cmNlcyA8PAogICAgL0ZvbnQgPDwKICAgICAgL0YxIDQgMCBSIAogICAgPj4KICA+PgogIC9Db250ZW50cyA1IDAgUgo+PgplbmRvYmoKCjQgMCBvYmoKPDwKICAvVHlwZSAvRm9udAogIC9TdWJ0eXBlIC9UeXBlMQogIC9CYXNlRm9udCAvVGltZXMtUm9tYW4KPj4KZW5kb2JqCgo1IDAgb2JqICAlIHBhZ2UgY29udGVudAo8PAogIC9MZW5ndGggNDQKPj4Kc3RyZWFtCkJUCjcwIDUwIFRECi9GMSAxMiBUZgooSGVsbG8sIHdvcmxkISkgVGoKRVQKZW5kc3RyZWFtCmVuZG9iagoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNSBmIAowMDAwMDAwMDEwIDAwMDAwIG4gCjAwMDAwMDAwNzkgMDAwMDAgbiAKMDAwMDAwMDE3MyAwMDAwMCBuIAowMDAwMDAwMzAxIDAwMDAwIG4gCjAwMDAwMDAzODAgMDAwMDAgbiAKdHJhaWxlcgo8PAogIC9TaXplIDYKICAvUm9vdCAxIDAgUgo+PgpzdGFydHhyZWYKNDkyCiUlRU9G';
+var zoom = 1;
+var brojStrane = 1;
+var fajl_url = 'ctut.pdf';
+var platno = null;
+var podloga = null;
+var mesto_za_tekst = null;
 
-var scale = 1; // zoom
-var platno = document.getElementById("platno");
-var podloga = platno.getContext("2d");
-var mesto_za_tekst = document.getElementById("mesto_za_tekst");
+window.addEventListener('load', function () {
+  platno = document.getElementById("platno");
+  podloga = platno.getContext("2d");
+  mesto_za_tekst = document.getElementById("mesto_za_tekst");
+  ucitajPdf(fajl_url);
+});
 
-var pdfUint8Array = prevedi_base64Strunu_u_uint8Array(pdfBase64);
-ucitajPdf(pdfUint8Array);
+document.addEventListener('click', function (e) {
+  var element = e.target;
+  if (element.classList.contains('js-idi-nazad')) idiNazad();
+  if (element.classList.contains('js-idi-napred')) idiNapred();
+}); // on click
+
 
 /* FUNKCIJE */
 
-function prevedi_base64Strunu_u_uint8Array(base64) {
-  var sirovo = atob(base64); // prirodna funkcija koja dekodira base64
-  var uint8Array = new Uint8Array(new ArrayBuffer(sirovo.length));
-  for (var i = 0; i < sirovo.length; i++) {
-    uint8Array[i] = sirovo.charCodeAt(i);
-  }
-  return uint8Array;
-}
-
-function ucitajPdf(pdfUint8Array) {
+function ucitajPdf(fajl_url) {
   PDFJS.disableWorker = true; // gasi workere zbog cross-origin greške
-  PDFJS.getDocument(pdfUint8Array).then(function renderujPdf(pdf) {
-    pdf.getPage(1).then(renderujStranu);
+  PDFJS.getDocument(fajl_url).then(function renderujPdf(pdf) {
+    pdf.getPage(brojStrane).then(renderujStranu);
   });
 }
 
-function renderujStranu(strana) {
-  var vidno_polje = strana.getViewport(scale); // namesta platno na velicinu vidnog polja
+function renderujStranu(pdfStrana) {
+  var vidno_polje = pdfStrana.getViewport(zoom); // namesta platno na velicinu vidnog polja
   platno.height = vidno_polje.height;
   platno.width = vidno_polje.width;
 
@@ -36,8 +38,8 @@ function renderujStranu(strana) {
   mesto_za_tekst.offsetTop = platno.offsetTop;
   mesto_za_tekst.offsetLeft = platno.offsetLeft;
 
-  strana.getTextContent().then(function (textContent) {
-    var tekst_lejer = new TextLayerBuilder(mesto_za_tekst, strana.number - 1); // broji od nule
+  pdfStrana.getTextContent().then(function (textContent) {
+    var tekst_lejer = new TextLayerBuilder(mesto_za_tekst, pdfStrana.number - 1); // broji od nule
     tekst_lejer.setTextContent(textContent);
 
     var render_podloga = {
@@ -45,6 +47,18 @@ function renderujStranu(strana) {
       viewport: vidno_polje,
       textLayer: tekst_lejer
     };
-    strana.render(render_podloga);
+    pdfStrana.render(render_podloga);
   });
+}
+
+function idiNazad() {
+  if (brojStrane <= 1) return;
+  brojStrane--;
+  renderujStranu(brojStrane);
+}
+
+function idiNapred() {
+  if (brojStrane >= ovajDokument.numPages) return;
+  brojStrane++;
+  renderujStranu(brojStrane);
 }
