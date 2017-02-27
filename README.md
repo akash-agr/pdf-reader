aff2ba126354ddde623259847b4d2a0d33dd05de
Damjan Pavlica <mudroljub@gmail.com>
2017-02-27 23:51:25 +0100

    obrisan vivin

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 PDF reader with text-selection, using [PDF.js](https://mozilla.github.io/pdf.js/) library.
 
-Koristan tutorijal: [Rendering a PDF with text-selection, using pdf.js](http://vivin.net/2013/06/06/rendering-a-pdf-with-text-selection-using-pdf-js/)
+Tutorial: [Rendering a PDF with text-selection, using pdf.js](http://vivin.net/2013/06/06/rendering-a-pdf-with-text-selection-using-pdf-js/)
 
-Izvorni [JSfiddle](http://jsfiddle.net/vivin/RjqUf/) je u međuvremenu umro.
+## Pokretanje
+
+Kloniraj repo i pokreni server na lokalu.
--- js/main.js
+++ js/main.js
@@ -1,87 +1,92 @@
-var zum = 1.0;
-var brojStrane = 1;
-var fajl_url = 'game-programming.pdf';
+/* global PDFJS */
 
-var drzac = null;
-var ovajDokument = null;
+let zum = 1.0
+let brojStrane = 1
+const fajl_url = 'game-programming.pdf'
 
-PDFJS.workerSrc = 'libs/pdfjs/pdf.worker.js';
-PDFJS.disableWorker = true;
+let drzac = null
+let ovajDokument = null
 
-window.addEventListener('load', function () {
-  drzac = document.getElementById('pdf-drzac');
-  ucitajPDF(fajl_url);
-});
+PDFJS.workerSrc = 'libs/pdfjs/pdf.worker.js'
+PDFJS.disableWorker = true // gasi workere zbog cross-origin greške
 
-document.addEventListener('click', function (e) {
-  var element = e.target;
-  if (element.classList.contains('js-idi-nazad')) okreniStranu(-1);
-  if (element.classList.contains('js-idi-napred')) okreniStranu(1);
-  if (element.classList.contains('js-zum')) zumiraj(0.1);
-  if (element.classList.contains('js-odzum')) zumiraj(-0.1);
-});
+/** FUNCTIONS **/
 
-
-function ucitajPDF(fajl_url) {
-  PDFJS.getDocument(fajl_url)
-    .then(function (pdf) {
-      ovajDokument = pdf;
-      if (brojStrane > ovajDokument.numPages) brojStrane = ovajDokument.numPages;
-      renderujStranu();
-    });
-}
-
-function renderujStranu() {
-  azurirajStanje();
-  ovajDokument.getPage(brojStrane)
-    .then(function (pdfStrana) {
-      var renderOpcije = {
-        container: drzac,
-        id: brojStrane,
-        scale: zum,
-        defaultViewport: pdfStrana.getViewport(zum),
-        textLayerFactory: new PDFJS.DefaultTextLayerFactory(),
-      };
-      var pdfPrikaz = new PDFJS.PDFPageView(renderOpcije);
-      pdfPrikaz.setPdfPage(pdfStrana);
-      pdfPrikaz.draw();
-    });
-}
-
-function azurirajStanje() {
-  proverBrojStrane();
-  brisiPrethodneStrane();
-  azurirajPolja();
+function $(selektor) {
+  return document.querySelector(selektor)
 }
 
 function proverBrojStrane() {
-  if (brojStrane < 1) brojStrane = 1;
-  if (brojStrane > ovajDokument.numPages) brojStrane = ovajDokument.numPages;
+  if (brojStrane < 1) brojStrane = 1
+  if (brojStrane > ovajDokument.numPages) brojStrane = ovajDokument.numPages
 }
 
 function brisiPrethodneStrane() {
-  var strane = document.querySelectorAll('.page');
-  for (var i = 0; i < strane.length; i++) {
-    strane[i].remove();
+  const strane = document.querySelectorAll('.page')
+  for (let i = 0; i < strane.length; i++) {
+    strane[i].remove()
   }
 }
 
 function azurirajPolja() {
-  $('#trenutna_strana').textContent = brojStrane;
-  $('#ukupno_strana').textContent = ovajDokument.numPages;
-  $('#zum').textContent = zum.toFixed(1);
+  $('#trenutna_strana').textContent = brojStrane
+  $('#ukupno_strana').textContent = ovajDokument.numPages
+  $('#zum').textContent = zum.toFixed(1)
+}
+
+function azurirajStanje() {
+  proverBrojStrane()
+  brisiPrethodneStrane()
+  azurirajPolja()
+}
+
+function renderujStranu() {
+  azurirajStanje()
+  ovajDokument.getPage(brojStrane)
+    .then(function(pdfStrana) {
+      const renderOpcije = {
+        container: drzac,
+        id: brojStrane,
+        scale: zum,
+        defaultViewport: pdfStrana.getViewport(zum),  // namesta platno na velicinu vidnog polja
+        textLayerFactory: new PDFJS.DefaultTextLayerFactory(),
+      }
+      const pdfPrikaz = new PDFJS.PDFPageView(renderOpcije)
+      pdfPrikaz.setPdfPage(pdfStrana)
+      pdfPrikaz.draw()
+    })
+}
+
+function ucitajPDF(fajl_url) {
+  PDFJS.getDocument(fajl_url)
+    .then(function(pdf) {
+      ovajDokument = pdf
+      if (brojStrane > ovajDokument.numPages) brojStrane = ovajDokument.numPages
+      renderujStranu()
+    })
 }
 
 function okreniStranu(broj) {
-  brojStrane += broj;
-  renderujStranu();
+  brojStrane += broj
+  renderujStranu()
 }
 
 function zumiraj(broj) {
-  zum += broj;
-  renderujStranu();
+  zum += broj
+  renderujStranu()
 }
 
-function $(selektor) {
-  return document.querySelector(selektor);
-}
+/** EVENTS **/
+
+window.addEventListener('load', function() {
+  drzac = document.getElementById('pdf-drzac')
+  ucitajPDF(fajl_url)
+})
+
+document.addEventListener('click', function(e) {
+  const element = e.target
+  if (element.classList.contains('js-idi-nazad')) okreniStranu(-1)
+  if (element.classList.contains('js-idi-napred')) okreniStranu(1)
+  if (element.classList.contains('js-zum')) zumiraj(0.1)
+  if (element.classList.contains('js-odzum')) zumiraj(-0.1)
+})
